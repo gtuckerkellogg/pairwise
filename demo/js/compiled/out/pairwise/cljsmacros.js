b51af8b2c72bd5273d11c41ce10b39cb6ed368c0
Greg Tucker-Kellogg <dbsgtk@nus.edu.sg>
2025-07-22 13:40:15 +0800

    removed compiled js, using github actions

--- demo/js/compiled/out/pairwise/cljsmacros.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.229 {:static-fns true, :optimize-constants true}
-goog.provide('pairwise.cljsmacros');
-goog.require('cljs.core');
--- demo/js/compiled/out/reagent/interop.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.229 {:static-fns true, :optimize-constants true}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
